6cf38c92aae452141d9376a4b6f14df316fad34c
Vamsi Krishna <vamc19@gmail.com>
2012-08-08 01:59:11 +0530

    Now works in Chrome, Safari, Firefox (3.6+) and IE (10)

--- themes/01/js/script.js
+++ themes/01/js/script.js
@@ -9,6 +9,8 @@ $(function(){
       lightswitch.css("backgroundPosition", "0 0");
       lightbulb.css("backgroundPosition", "0 0");
       container.css({background: '-webkit-gradient(radial, 50% 30%, 0, 50% 30%, 700, from(#959047), to(#000000))'});
+      container.css({background: '-moz-radial-gradient(circle, #959047, #000000)'});
+      container.css({background: '-ms-radial-gradient(circle, #959047, #000000)'});
     }else{
       lightbulb.addClass("off");
       lightswitch.css("backgroundPosition", "-80 0");
